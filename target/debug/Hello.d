608f4ddad7b7195c9dd4af0fb99621fbee7a8bb6
Your Name <Your Email>
2019-12-05 09:00:30 +0000

    added commit

---
+++ target/debug/Hello.d
@@ -0,0 +1,1 @@
+/home/nida/Hello/Hello/target/debug/Hello: /home/nida/Hello/Hello/src/main.rs
---
+++ target/debug/deps/Hello-0f1ffd9c2cf251c7.d
@@ -0,0 +1,5 @@
+/home/nida/Hello/Hello/target/debug/deps/Hello-0f1ffd9c2cf251c7: src/main.rs
+
+/home/nida/Hello/Hello/target/debug/deps/Hello-0f1ffd9c2cf251c7.d: src/main.rs
+
+src/main.rs:
